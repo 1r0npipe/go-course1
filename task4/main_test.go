fabf758e769e360c336f73b695d8affd3b5987c3
Eugene Makhinya <jesonsev@gmail.com>
2021-01-26 10:29:12 +0000

    fix for test file

--- task4/main_test.go
+++ task4/main_test.go
@@ -7,11 +7,14 @@ const N = 10
 
 func Test_insert_sort(t *testing.T) {
 	testsTable := []struct {
-		got  [N]int
-		want [N]int
-	} {
-		[N]int{4,6,2,8,0,3,10,56,9,1},
-		[N]int{0,1,2,3,4,6,8,9,10,56},
+		got  [N]int{}
+		want [N]int{}
+	}{
+		{4,6,2,8,0,3,10,56,9,1},
+		{0,1,2,3,4,6,8,9,10,56},
+	}{
+		{4,6,2,8,0,3,10,56,9,1},
+		{0,1,2,3,4,6,8,9,10,56},
 	}
 
 	for _,testCase := range testsTable {
