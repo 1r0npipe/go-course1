85fbf08c97f0b56e1cfec7cf9337ea79c4f68cec
Eugene Makhinya <jesonsev@gmail.com>
2021-02-03 13:21:19 +0000

    Task with pointers example

---
+++ task6/task6.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	fmt.Println("Actions with pointers")
+	a := 1
+	b := 2
+	c := unsafe.Pointer(&a)
+	d := unsafe.Pointer(&b)
+	fmt.Printf("This is a = %d, b = %d, c = %d, d = %d, &a = %p,  &b = %p, *c = %v, *d = %v\n", 
+				a, b, c, d, &a, &b, *(*int)(c), *(*int)(d))
+	temp := uintptr(d) - uintptr(c)
+	if (temp > 0) {
+		fmt.Printf("Getting b via pointer to a = %v\n",*(*int)(unsafe.Pointer(uintptr(c) + temp)))
+		fmt.Printf("Getting a via pointer to b = %v",*(*int)(unsafe.Pointer(uintptr(d) - temp)))
+	}
+}
