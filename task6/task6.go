8e07978e8fe5a47a6ab07b40170473797cffa50c
Eugene Makhinya <jesonsev@gmail.com>
2021-02-04 16:58:58 +0000

    last commit

--- task6/task6.go
+++ task6/task6.go
@@ -14,6 +14,7 @@ func main() {
 	fmt.Printf("This is a = %d, b = %d, c = %d, d = %d, &a = %p,  &b = %p, *c = %v, *d = %v\n", 
 				a, b, c, d, &a, &b, *(*int)(c), *(*int)(d))
 	temp := uintptr(d) - uintptr(c)
+	fmt.Printf("Size of a = %d and b = %d\n", unsafe.Sizeof(a), unsafe.Sizeof(b))
 	if (temp > 0) {
 		fmt.Printf("Getting b via pointer to a = %v\n",*(*int)(unsafe.Pointer(uintptr(c) + temp)))
 		fmt.Printf("Getting a via pointer to b = %v",*(*int)(unsafe.Pointer(uintptr(d) - temp)))
