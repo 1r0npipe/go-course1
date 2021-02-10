3426e60fb4873bcf8ce7858c7760c7da8fbb9f19
Eugene Makhinya <jesonsev@gmail.com>
2021-02-10 20:31:33 +0000

    bubbleSort added

--- task4/main.go
+++ task4/main.go
@@ -20,9 +20,24 @@ func insertSort(array [n]int) [n]int {
 	return array
 }
 
+//bubbleSort
+func bubbleSort(array [n]int) [n]int {
+	arrayLen := len(array) 
+	for i := 0; i < arrayLen - 1; i++ {
+		for j := 0; j < arrayLen - 1; j++ {
+			if (array[j] > array[j+1]) {
+				array[j], array[j + 1] = array[j + 1], array[j]
+			}
+		}
+	}
+	return array
+}
+
 func main() {
 	testArray := [n]int{4, 6, 5, 3, 6, 7, 4, 0, 9, 10}
+	testArray2 := [n]int{4, 6, 5, 3, 6, 7, 4, 0, 9, 10}
 
 	fmt.Println(insertSort(testArray))
 	sliceTask()
+	fmt.Println(bubleSort(testArray2))
 }
